8f05eb977dfd6e2a6c38062ff711fab55e3ef042
Denis Pauk <pauk.denis@gmail.com>
2019-12-01 13:34:05 +0200

    minimize differences with upstream in synopsys/dw-hdmi

--- include/drm/bridge/dw_hdmi.h
+++ include/drm/bridge/dw_hdmi.h
@@ -160,11 +160,6 @@ struct dw_hdmi_plat_data {
 			     const struct dw_hdmi_plat_data *pdata,
 			     unsigned long mpixelclock);
 
-	unsigned long (*get_input_bus_format)(void *data);
-	unsigned long (*get_output_bus_format)(void *data);
-	unsigned long (*get_enc_in_encoding)(void *data);
-	unsigned long (*get_enc_out_encoding)(void *data);
-
 	/* Vendor Property support */
 	const struct dw_hdmi_property_ops *property_ops;
 };
